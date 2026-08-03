cef3b400305a69b4a120fa46590195e8bfe86b63
Sushant Sonawane <sushantsonawanex1@gmail.com>
2026-08-03 10:27:27 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+new Typed(".typing",{
+
+strings:[
+"Python Developer",
+"AI Enthusiast",
+"Full Stack Learner",
+"Open Source Contributor"
+],
+
+typeSpeed:60,
+
+backSpeed:40,
+
+loop:true
+
+});
